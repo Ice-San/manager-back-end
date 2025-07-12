ff0872267f929dee91f4f0fbedcbf5f00491e5e8
Ice-San <rotommine@gmail.com>
2025-07-12 01:36:47 +0100

    MAN-19 | Back-End | Adapt API to Support Render!

--- src/db/config.ts
+++ src/db/config.ts
@@ -5,20 +5,15 @@ const {
     DB_HOST,
     DB_NAME,
     DB_PASSWORD,
-    DB_PORT,
-    RAILWAY_DB_USER,
-    RAILWAY_DB_HOST,
-    RAILWAY_DB_NAME,
-    RAILWAY_DB_PASSWORD,
-    RAILWAY_DB_PORT
+    DB_PORT
 } = process.env;
 
 const client = new pg.Client({
-    user: RAILWAY_DB_USER || DB_USER,
-    host: RAILWAY_DB_HOST || DB_HOST,
-    database: RAILWAY_DB_NAME || DB_NAME,
-    password: RAILWAY_DB_PASSWORD || DB_PASSWORD,
-    port: Number(RAILWAY_DB_PORT || DB_PORT),
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASSWORD,
+    port: Number(DB_PORT),
 });
 
 client
--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,15 @@ import client from './db/config';
 import usersRoutes from './api/routes/users';
 import authRoutes from './api/routes/auth';
 
+const { 
+    PORT, 
+    RENDER_PUBLIC_DOMAIN, 
+    RAILWAY_PUBLIC_DOMAIN 
+} = process.env;
+
 const app = express();
-const port = process.env.PORT || 3000;
-const domain = process.env.RAILWAY_PRIVATE_DOMAIN || "localhost";
+const port = PORT || 3000;
+const domain = RAILWAY_PUBLIC_DOMAIN || RENDER_PUBLIC_DOMAIN || "localhost";
 
 app.use(cors());
 app.use(express.json());
@@ -22,5 +28,19 @@ app.use('/auth', authRoutes);
 
 app.listen(port, () => {
     client;
-    console.log(`App is online at: http://${domain}`);
+
+    let url;
+    switch (domain) {
+        case RAILWAY_PUBLIC_DOMAIN:
+            url = `Public URL (Railway): https://${RAILWAY_PUBLIC_DOMAIN}`;
+            break;
+        case RENDER_PUBLIC_DOMAIN:
+            url = `Public URL (Render): ${RENDER_PUBLIC_DOMAIN}`;
+            break;
+        default:
+            url = `Local URL: http://localhost:${port}`;
+            break;
+    }
+
+    console.log(`App is online at: ${url}`);
 });

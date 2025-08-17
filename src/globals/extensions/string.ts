1464cba7e93e4e498e456fac4dc03f7a1bd18f62
Ice-San <rotommine@gmail.com>
2025-08-17 01:27:12 +0100

    Back-End | Server Test!

--- src/globals/extensions/string.ts
+++ src/globals/extensions/string.ts
@@ -4,7 +4,7 @@ declare global {
     }
 }
 
-String.prototype.toCapitalize = (): string => {
+String.prototype.toCapitalize = function(): string {
     const str = String(this);
     if(str.length === 0) return '';
     return str.charAt(0).toUpperCase() + str.slice(1);
--- src/helpers/getDomain.ts
+++ src/helpers/getDomain.ts
@@ -1,7 +1,8 @@
 const { 
     PORT, 
     RENDER_PUBLIC_DOMAIN, 
-    RAILWAY_PUBLIC_DOMAIN 
+    RAILWAY_PUBLIC_DOMAIN,
+    NEON_PUBLIC_DOMAIN
 } = process.env;
 
 const port = PORT || 3000;
@@ -19,7 +20,7 @@ const data: domain = {
 };
 
 export const getDomain = () => {
-    const domain = RAILWAY_PUBLIC_DOMAIN || RENDER_PUBLIC_DOMAIN || "localhost";
+    const domain = NEON_PUBLIC_DOMAIN || RAILWAY_PUBLIC_DOMAIN || RENDER_PUBLIC_DOMAIN || "localhost";
     switch (domain) {
         case RAILWAY_PUBLIC_DOMAIN:
             data.type = "public";
@@ -30,6 +31,11 @@ export const getDomain = () => {
             data.type = "public";
             data.provider = "render";
             data.url = `${RENDER_PUBLIC_DOMAIN}`;
+            break;
+        case NEON_PUBLIC_DOMAIN:
+            data.type = "public";
+            data.provider = "neon";
+            data.url = `${NEON_PUBLIC_DOMAIN}`;
             break;
         default:
             data.type = "private";
